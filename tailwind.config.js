7a9314e6cf93a60efa9b36d5749c212ae911116f
JoyLinton04 <adujoy05@gmail.com>
2023-12-18 14:50:39 +0000

    added custom screens for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'header': ['M PLUS Code Latin', 'monospace'],
+      },
+      screens: {
+        '2xxl': {'max': '1535px'},
+        'xxl': {'max': '1279px'},
+        'lgg': {'max': '1024px'},
+        'mdd': {'max': '800px'},
+        'ssm': {'max': '639px'},
+      }
+    },
   },
   plugins: [],
 }
